bba5c97af5252bc320aaba018fb8a2ecbf2c75c5
HuyTran <trandoanhuy.jvb@gmail.com>
2023-04-18 09:50:06 +0700

    fix prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    arrowParens: "always",
-    semi: false,
-    trailingComma: "none",
-    tabWidth: 2,
-    endOfLine: "auto",
-    useTabs: false,
-    singleQuote: true,
-    printWidth: 120,
-    jsxSingleQuote: true,
-};
+  arrowParens: 'always',
+  semi: false,
+  trailingComma: 'none',
+  tabWidth: 2,
+  endOfLine: 'auto',
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 120,
+  jsxSingleQuote: true
+}
